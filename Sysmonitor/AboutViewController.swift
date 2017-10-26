bad453ead05e9b18de4656d09cbd16186f403fc5
Tou Ka <ka_tou@outlook.com>
2017-10-26 17:09:41 +0800

    Test version - 17-10-25-2

--- Sysmonitor/AboutViewController.swift
+++ Sysmonitor/AboutViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class AboutViewController: UIViewController {
 
     @IBOutlet weak var btnExit: UIButton!
+    @IBOutlet weak var btnAbout: UIButton!
+    @IBOutlet weak var btnCall: UIButton!
+    @IBOutlet weak var btnGuide: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +27,23 @@ class AboutViewController: UIViewController {
     
     @IBAction func btnClick(_ sender: Any) {
         exit(0)
+    }
+    
+    @IBAction func abtClick(_ sender: Any) {
+        let aboutAlert = UIAlertController(title: "关于", message: "Copyright © 2011-2017 Sawatari Inc. All rights reserved.",preferredStyle: .alert)
+        let aboutCancel = UIAlertAction(title: "确定", style: .destructive, handler: nil)
+        aboutAlert.addAction(aboutCancel)
+        self.present(aboutAlert, animated: true, completion: nil)
+    }
+    
+    @IBAction func callClick(_ sender: Any) {
+        UIApplication.shared.openURL(URL(string: "telprompt://+8618655629450")!)
+    }
+    
+    @IBAction func guiClick(_ sender: Any) {
+        let guideAlert = UIAlertController(title: "使用说明", message: "本简易APP可用于监测手机各部件工作状态，以及查询手机内部配置、网络状况等情况。",preferredStyle: .alert)
+        let guideCancel = UIAlertAction(title: "确定", style: .destructive, handler: nil)
+        guideAlert.addAction(guideCancel)
+        self.present(guideAlert, animated: true, completion: nil)
     }
 }
--- Sysmonitor/SecondViewController.swift
+++ Sysmonitor/SecondViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var mainScroll: UIScrollView!
+    @IBOutlet weak var detailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 详细信息卡片初始化
+        detailLabel.text = String(format: "大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father\n大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹大儿啊我是你爹\nすみません、俺はお前の父上だぜ、覚悟しなさい\nDear my son, I'm your father")
+        
+        // 设置mainScroll属性
+        mainScroll.autoresizingMask = UIViewAutoresizing.flexibleHeight
+        mainScroll.isScrollEnabled = true
+        mainScroll.showsHorizontalScrollIndicator = true
     }
 
     override func didReceiveMemoryWarning() {
